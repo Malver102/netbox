d3e7d72049573f6688fad0c2afbaafc9e86a2075
PAWEL SUSZKO <C:\Users\suspaw\AppData\Roaming\The Bat!>
2023-12-20 14:54:16 +0100

    test

--- config/psql.sh
+++ config/psql.sh
@@ -7,7 +7,7 @@ DB_USER_PASS=netbox
 service postgresql start
 
 psql -U postgres  -c "CREATE DATABASE $DB_NAME;"
-psql -U postgres  -c "CREATE USER $DB_USER WITH PASSWORD $DB_USER_PASS;"
-psql -U postgres  -c "ALTER DATABASE $DB_NAME OWNET TO $DB_USER_PASS;"
-psql -U postgres  -c "connect $DB_NAME;"
-psql -U postgres  -c "GRANT CREATE ON SCHEMA public TO $DB_NAME;"
+psql -U postgres  -c "CREATE USER $DB_USER WITH PASSWORD '$DB_USER_PASS';"
+psql -U postgres  -c "ALTER DATABASE $DB_NAME OWNER TO $DB_USER_PASS;"
+psql -U postgres  -c "/connect $DB_NAME;"
+psql -U postgres  -c "GRANT CREATE ON SCHEMA public TO $DB_USER;"
