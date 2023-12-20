95ce46e8e59f2e315166cf016805b07e8d518bf7
PAWEL SUSZKO <C:\Users\suspaw\AppData\Roaming\The Bat!>
2023-12-20 14:50:16 +0100

    test

--- config/psql.sh
+++ config/psql.sh
@@ -9,5 +9,5 @@ service postgresql start
 psql -U postgres  -c "CREATE DATABASE $DB_NAME;"
 psql -U postgres  -c "CREATE USER $DB_USER WITH PASSWORD $DB_USER_PASS;"
 psql -U postgres  -c "ALTER DATABASE $DB_NAME OWNET TO $DB_USER_PASS;"
-psql -U postgres  -c "grant all privileges on database $DB_NAME to $DB_USER;"
+psql -U postgres  -c "connect $DB_NAME;"
 psql -U postgres  -c "GRANT CREATE ON SCHEMA public TO $DB_NAME;"
