425cabddca075317dd7c433c89f1233ab641ec7f
PAWEL SUSZKO <C:\Users\suspaw\AppData\Roaming\The Bat!>
2023-12-20 10:49:56 +0100

    test

--- config/psql.sh
+++ config/psql.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
-
-set -e
 DB_NAME="netbox"
 DB_USER="netbox_db"
 DB_USER_PASS="netbox"
 
-psql <<EOF
-createdb  $DB_NAME;
-psql -c "CREATE USER $DB_USER WITH PASSWORD '$DB_USER_PASS';"
-psql -c "ALTER DATABASE '$DB_NAME' OWNET TO '$DB_USER_PASS';"
-psql -c "grant all privileges on database $DB_NAME to $DB_USER;"
-psql -c "GRANT CREATE ON SCHEMA public TO '$DB_NAME';"
-echo "Postgres User '$DB_USER' and database '$DB_NAME' created."
-EOF
+
+psql -U amdin "CREATE USER $DB_USER WITH PASSWORD '$DB_USER_PASS';"
+psql -U admin "ALTER DATABASE '$DB_NAME' OWNET TO '$DB_USER_PASS';"
+psql -U admin "grant all privileges on database $DB_NAME to $DB_USER;"
+psql -U admin "GRANT CREATE ON SCHEMA public TO '$DB_NAME';"
