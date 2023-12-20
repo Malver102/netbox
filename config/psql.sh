a712da92f54f3bda5b97a527838b524f1ef4eae0
PAWEL SUSZKO <C:\Users\suspaw\AppData\Roaming\The Bat!>
2023-12-20 10:38:43 +0100

    test

--- config/psql.sh
+++ config/psql.sh
@@ -6,7 +6,7 @@ DB_NAME="netbox"
 DB_USER="netbox_db"
 DB_USER_PASS="netbox"
 
-postgres <<EOF
+psql <<EOF
 createdb  $DB_NAME;
 psql -c "CREATE USER $DB_USER WITH PASSWORD '$DB_USER_PASS';"
 psql -c "ALTER DATABASE '$DB_NAME' OWNET TO '$DB_USER_PASS';"
