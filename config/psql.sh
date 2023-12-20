ae69440338b523e1ff0a514aa5be9465980fe680
PAWEL SUSZKO <C:\Users\suspaw\AppData\Roaming\The Bat!>
2023-12-20 11:05:00 +0100

    test

--- config/psql.sh
+++ config/psql.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-DB_NAME="netbox"
-DB_USER="netbox_db"
+DB_NAME="netbox_db"
+DB_USER="netbox"
 DB_USER_PASS="netbox"
 
-
-psql -U postgres "CREATE USER $DB_USER WITH PASSWORD '$DB_USER_PASS';"
-psql -U postgres "ALTER DATABASE '$DB_NAME' OWNET TO '$DB_USER_PASS';"
-psql -U postgres "grant all privileges on database $DB_NAME to $DB_USER;"
-psql -U postgres "GRANT CREATE ON SCHEMA public TO '$DB_NAME';"
+psql -U postgres -c "CREATE DATABASE $DB_NAME;"
+psql -U postgres -c "CREATE USER $DB_USER WITH PASSWORD '$DB_USER_PASS';"
+psql -U postgres -c "ALTER DATABASE '$DB_NAME' OWNET TO '$DB_USER_PASS';"
+psql -U postgres -c "grant all privileges on database $DB_NAME to $DB_USER;"
+psql -U postgres -c "GRANT CREATE ON SCHEMA public TO '$DB_NAME';"
